a8289ce7a391da450574fae41320a9102306d4c4
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-05-26 21:33:04 -0700

    psuedocoding

---
+++ server.js
@@ -0,0 +1,16 @@
+// User begins application
+// user is prompted with options: 
+// 1. View all employees with respective departments and roles
+// 2. Add an employee along with their role and department
+// 3. Edit an employee's role
+
+// when user selects to view all employees
+// console log current table of all current employees
+
+// when user selects to add an employee
+// user is prompted to input a first name
+// user is prompted to input last name
+// user is prompted to select role from a list of roles
+// user is prompted to select employees manager from list of current employees
+// if user doesn't have a manager, default value is null
+
