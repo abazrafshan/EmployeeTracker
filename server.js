f94d4b92da051df005d69d456bea1bbfd17a52ab
Arash Bazrafshan <abazrafshan123@gmail.com>
2020-05-29 09:49:52 -0700

    Added comments to server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const mysql = require("mysql");
 const cTable = require("console.table");
 const inquirer = require("inquirer"); 
-var rolesArray = ["Sales Lead", "Salesperson", "Software Engineer", "Lead Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"];
-var departmentsArray =["Engineering", "Accounting", "Legal", "Sales"];
 
 var connection = mysql.createConnection({
     hose: "localhost",
@@ -11,12 +9,12 @@ var connection = mysql.createConnection({
     password: "root",
     database: "employeeDB"
 });
-
+// When connection is made to database promptUser function is called to initiate application
 connection.connect(err => {
     if (err) throw err;
     promptUser();
 });
-
+// Inquirer method called to present user with list of options for how to proceed with the application
 function promptUser(){
     inquirer.prompt({
         type: "list",
@@ -33,43 +31,53 @@ function promptUser(){
             "Exit"
         ]
     }).then(data => {
+        // Call fuction based on user's input to inquirer method
         switch(data.options){
             case "View all employees":
+                // Shows current list of employees
                 viewEmployees();
                 break;
 
             case "View all roles":
+                // Shows current list of job roles
                 viewRoles();
                 break;
 
             case "View all departments":
+                // Shows current list of departments
                 viewDepartments();
                 break;
 
             case "Add employee":
+                // User can add employee to team
                 addEmployee();
                 break;
 
             case "Add role":
+                // User can add a new role
                 addRole();
                 break;
 
             case "Add department":
+                // User can add a new department
                 addDepartment();
                 break;
 
             case "Update employee role":
+                // User can update an employee's job role
                 updateRole();
                 break;
 
             case "Exit":
+                // End connection
                 connection.end();
                 break;
         }
     });
 };
-
+// Query ties together the roles, departments, and employees sql tables an displays this to user in terminal in table format
 function viewEmployees(){
+    // Displays employees' first names, last names, manager id, title, salary, and department they belong to
     var query = "select first_name,last_name,manager_id,title,salary,department_name from employees inner join roles on roles.id = employees.role_id inner join departments on departments.id = roles.department_id";
     connection.query(query,(err,res) => {
         if (err) throw err;
@@ -77,26 +85,30 @@ function viewEmployees(){
         promptUser();
     })
 };
-
+// Query ties together roles and departments sql tables and displays list of roles defined in application
 function viewRoles(){
+    // Displays role id, role title, department the role belongs to
     var query = "select roles.id, title, department_name from roles inner join departments on departments.id=roles.department_id";
     connection.query(query,(err,res) => {
         if (err) throw err;
         console.table(res);
+        // Prompt user with list of options 
         promptUser();
     })
 };
-
+// Query displays all columns in departments table
 function viewDepartments(){
     var query = "select * from departments";
     connection.query(query,(err,res) => {
         if (err) throw err;
+        // Display table to terminal 
         console.table(res);
         promptUser();
     })
 };
-
+// Function adds employees to employee roster
 function addEmployee(){
+    // Inquirer method called for user to store new employees' name, role, and manager id
     inquirer.prompt([
         {
             type: "input",
@@ -119,8 +131,10 @@ function addEmployee(){
             name: "managerid"
         },
     ]).then(data => {
+        // Query inserts values employees table
         var query = "INSERT INTO employees SET ?";
         connection.query(query, 
+            // Set name, role, and manager id 
             {first_name: data.firstname,
             last_name: data.lastname, 
             role_id: data.roleid,
@@ -132,82 +146,76 @@ function addEmployee(){
         })    
     })
 }
-// 3
-// when user selects to edit an employee role
-// console log current table of all current employees
-// user is prompted to select employee from list of current emloyees
-// when desired employee selected, console log row that displays employees current role and department
-// User prompted to select new role for emplployee from list of existing roles
-// console log updated table of employees with updated role
-// prompt user with options again
 
 function updateRole(){
+    // Query employees table
     var query = "select * from employees";
-    // "select employees.id, first_name,last_name,title,department_name from employees inner join roles on roles.id = employees.role_id inner join departments on departments.id = roles.department_id";
     connection.query(query, (err,results) => {
         if (err) throw err;
-        // console.table(results);
+        // Inquirer method called for user to select which employee role to modify
         inquirer.prompt([
             {
                 type: "list",
                 name: "choice",
                 message: "Whose role would you like to modify",
                 choices: ()=> {
+                    // Declare empty choiceArray
                     var choiceArray = [];
+                    // Loop through results from sql query 
                     for (var i = 0; i < results.length; i++){
+                        // Push list of results to choiceArray, pushing the first name, last name, and eid of each employee 
                         choiceArray.push({name: `${results[i].first_name} ${results[i].last_name}`, value: `${results[i].eid}`});
                     }
-                    console.log(choiceArray);
+                    // Return choiceArray
                     return choiceArray;
                 },
             }
         ]).then(answer => {
+            // Store user's input to inquirer method
             var eid = answer.choice;
+            // Query ties roles and departments tables together
             var query = "select roles.id AS rid, roles.title, roles.department_id AS did from roles inner join departments on roles.department_id = departments.id";
             connection.query(query, (err, res) => {
-                console.log(res)
                 if (err) throw err;
+                // Inquirer method called for user to select which role to assign to new employee
                 inquirer.prompt([
                 {
                     type: "rawlist",
                     name: "updaterole",
                     message: "What is their new role?",
                     choices: () => {
+                        // Declare empty rolesArray
                         var rolesArray = [];
+                        // Loop through results from sql query 
                         for (var i = 0; i < res.length; i++){
                             rolesArray.push({name: `${res[i].title}`, value: {rid :`${res[i].rid}`,  did: `${res[i].did}`}});
                         }
+                        // Return rolesArray
                         return rolesArray;
-                        // console.log(rolesArray);
                     },
                 }
             ]).then((result) => {
-            // var chosenemployee = answer.choice;
-            // console.log(chosenemployee);
-            console.log("this is working")
-                console.log(result);
+                // Query accesses employees table and sets role id
                 var query = "update employees set ? where ?";
                 connection.query(query, 
-                        [{
-                            role_id: result.updaterole.rid
-                        // department_id: result.updaterole.did
-                        
-                    }, 
-                        {eid: eid}],
-                        function (err, res) {
+                    // Set role and employee id's
+                    [{role_id: result.updaterole.rid}, {eid: eid}],
+                    function (err, res) {
                     if (err) throw err;
                     console.log("Update successful");
+                    // View employees then prompt user 
                     viewEmployees();
+                    promptUser();
                 }
                 );
             }) ;
             });
         });
-        // promptUser();
     });
 }
-
+// Add role to list of job roles
 function addRole(){
+    // Inquirer method called to gather title, salary, and department the new role belongs to
     inquirer.prompt([
         {
             type: "input",
@@ -225,7 +233,9 @@ function addRole(){
             name: "newroledepartment"
         }
     ]).then(data => {
+        // Query accesses roles table
             var query = "INSERT INTO roles SET ?";
+            // Set title, salary, department id 
             connection.query(query, {
                 title: data.newroletitle,
                 salary: data.newrolesalary,
@@ -238,14 +248,17 @@ function addRole(){
             })
         })
 };
-
+// Adds department to list of departments
 function addDepartment(){
+    // Inquirer method called to gather name of new department
     inquirer.prompt({
         type: "input",
         message: "Enter the name of the department you wish to add, department id will be auto-generated",
         name: "newdepartment"
     }).then(data => {
+        // Query accesses departments table
         var query = "INSERT INTO departments SET ?";
+        // Set department name 
         connection.query(query, {department_name: data.newdepartment},
         (err,res) => {
             if (err) throw err;
@@ -253,5 +266,4 @@ function addDepartment(){
             promptUser();
         })
     })
-
 };
